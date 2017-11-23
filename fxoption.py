1158725dc160bf36aad9ecdcb82e5af4d7ff4fa2
joshgiersch <josh@josh.sg>
2017-11-23 09:53:54 -0800

    New function fxoptionprice

---
+++ fxoption.py
@@ -0,0 +1,15 @@
+from scipy.stats import norm
+import math
+
+
+def fxoptionprice(s, k, rf, rd, vol, t, iscall):
+    d1 = (math.log(s/k) + (rd-rf+vol**2/2)*t)/(vol*math.sqrt(t))
+    d2 = d1-vol*math.sqrt(t)
+    if iscall:
+        price = s * math.exp(-rf * t) * norm.cdf(d1) - k * math.exp(-rd * t) * norm.cdf(d2)
+    else:
+        price = k * math.exp(-rd * t) * norm.cdf(-d2) - s * math.exp(-rf * t) * norm.cdf(-d1)
+    return price
+
+
+print(fxoptionprice(60,65,0,0.08,0.3,0.25,True))
